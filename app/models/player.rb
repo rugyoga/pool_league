17e0a07d3d65da9abccd265c79875eeeabf3b4d1
Guy Argo <guy.argo@springshot.com>
2016-07-20 09:42:30 -0700

    wip

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,11 @@
 class Player < ActiveRecord::Base
   belongs_to :team
   has_many :home_games, class_name: 'Game', foreign_key: :home_player_id
-  has_many :away_games, class_name: 'Game', foreign_key: :home_player_id
+  has_many :away_games, class_name: 'Game', foreign_key: :away_player_id
 
   def wins
-    home_games.where( home_player_won: true  ) +
-    away_games.where( home_player_won: false )
+    @wins ||= home_games.where( home_player_won: true  ) +
+              away_games.where( home_player_won: false )
   end
 
   def no_of_wins
@@ -13,19 +13,31 @@ class Player < ActiveRecord::Base
   end
 
   def losses
-    home_games.where( home_player_won: false ) +
-    away_games.where( home_player_won: true  )
+    @losses ||= home_games.where( home_player_won: false ) +
+                away_games.where( home_player_won: true  )
   end
 
   def no_of_losses
-    wins.size
+    losses.size
+  end
+
+  def win_percentage
+    games.size == 0 ? 0.0 : (100.0*no_of_wins.to_f)/no_of_games.to_f
   end
 
   def games
-    (home_games + away_games).sort_by(&:id)
+    @games ||= (home_games + away_games).sort_by(&:id)
+  end
+
+  def no_of_games
+    games.size
   end
 
   def table_runs
-    wins.where( table_run: true )
+    @table_runs ||= wins.select(&:table_run)
+  end
+
+  def no_of_table_runs
+    table_runs.size
   end
 end
